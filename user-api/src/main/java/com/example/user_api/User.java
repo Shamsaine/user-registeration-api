e25c384eeb4ba923a6ed07a214fd59db4e06f505
Shamsudeen Ibrahim <shamsain3@gmail.com>
2025-02-25 14:39:08 +0100

    added the model layer to structure user table

--- user-api/src/main/java/com/example/user_api/User.java
+++ user-api/src/main/java/com/example/user_api/User.java
@@ -1,6 +1,52 @@
-package main.java.com.example.user_api;
+package com.example.user_api;
 
+import jakarta.persistence.*;
 
+@Entity
+@Table(name="users")
 public class User {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @Column(nullable = false, length = 255)
+    private String firstName;
+
+    @Column (nullable = false, length = 255)
+    private String lastName;
+
+    @Column (nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    @Column(nullable = true, length = 255)
+    private String phoneNumber;
+
+    public User(){}
+
+    public User(String firstName, String lastName, String email, Gender gender, String phoneNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+    }
+    
+    //getters
+    public void getUserId(){return userId;}
+
+    public void getFirstName(){return firstName;}
+
+    public void getLastName(){return lastName;}
+
+    public void getEmail(){return email;}
+
+    public void getGender(){return gender;}
+
+    public void getPhoneNumber(return phoneNumber;)
+
 }
