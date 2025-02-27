a595e42e17dc570a48f9e5834a31e1e06853fcc4
Shamsudeen Ibrahim <shamsain3@gmail.com>
2025-02-27 18:14:39 +0100

    fixed a bunch of compilation errors

--- user-api/src/main/java/com/example/user_api/UserRepository.java
+++ user-api/src/main/java/com/example/user_api/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, int> {
     Optional<User> findByUserId(int userId);
 }
